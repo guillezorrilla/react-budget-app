2c05bc53a8ba54abba7bea1b0306310e3123b1a6
Guillermo Zorrilla <guillez94@hotmail.com>
2019-10-31 21:09:06 +0100

    Fisrt commit for budget app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+import AppRouter from './routers/AppRouter';
+import { Provider} from 'react-redux' 
+import {addExpense} from './actions/expenses'
+import getVisibleExpenses from './selectors/expenses'
+import configureStore from './store/configureStore'
+
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water Bill', amount: 4500}))
+store.dispatch(addExpense({description: 'Gas Bill', createdAt: 1000}))
+store.dispatch(addExpense({description: 'Rent', amount: 65000}))
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
+const jsx = (
+	<Provider store = {store}>
+		<AppRouter />
+	</Provider>
+)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{jsx}
+		</div>
+	);
 }
 
 export default App;
